8b0059e2a531d1c16a198b11a9ef09f255fd82cf
unknown <dnl.rammer@gmail.com>
2017-02-16 01:29:25 +0100

    added screen ratio/scaling feature on client

--- Assets/AudienceExtension/NetworkClientGaze.cs
+++ Assets/AudienceExtension/NetworkClientGaze.cs
@@ -4,7 +4,23 @@ using UnityEngine;
 
 [RequireComponent(typeof(GazePlotter))]
 public class NetworkClientGaze : MonoBehaviour {
+    private static NetworkClientGaze instance;
+    public static NetworkClientGaze Instance { get { return instance; } }
+
+    // scaling defines the ratio between the screen
+    // - which shows the game and 
+    // - the screen of the notebook on which the eyetracker is attached
+    // e.g 24 / 17.4 = 1.37931
+    public float scaling = 1;
+
     private GazePlotter gazePlotter;
+
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
 
     void Start()
     {
@@ -16,6 +32,7 @@ public class NetworkClientGaze : MonoBehaviour {
 
     void Update()
     {
+
         if (Communicator.Player == null)
             return;
 
